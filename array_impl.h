66023dd64c6a2b7bd0a6f4e755413a2463428893
benbirchpersonal <93481691+benbirchpersonal@users.noreply.github.com>
2021-11-02 08:13:36 +0000

    Update array_impl.h

--- array_impl.h
+++ array_impl.h
@@ -6,56 +6,52 @@
 #include <stdio.h>
 #include <cassert>
 
-constexpr auto MAX_ARRAYSIZE = 100;
+constexpr size_t MAX_ARRAYSIZE = 100;
 static char ARRAY_OPERATION_BUFFER[MAX_ARRAYSIZE];
 
 
-template<class T>
+template<class _T>
 class arr {
 public:
-	arr (int size);
-	void print();
-	T& operator[](int i);
+	// constructors
+	/*
+	* defualt constructor, type specified in template argument list <here>()
+	*/
+	arr(size_t size);
+	// operators
+
+	/*
+	* index access
+	*/
+	_T& operator[](size_t i);
+
 private:
 	size_t _ElemSize;
 	size_t _ArraySize;
 	void* _ArrayLocaton;
 };
 
-template<class T>
-inline T& arr<T>::operator[](int i)
-{
-	assert(i < _ArraySize);
-	T temp = *(T*)(malloc(sizeof(T)));
-	memcpy((T*)&temp, ((T*)_ArrayLocaton + sizeof(T) * i), sizeof(T));
-	return temp;
-}
 
-template<class T>
-inline arr<T>::arr(int size)
+
+/*
+* Constructor
+*/
+template<class _T>
+inline arr<_T>::arr(size_t size)
 {
 	assert(size < MAX_ARRAYSIZE);
-	_ElemSize = sizeof(T);
+	_ElemSize = sizeof(_T);
 	_ArraySize = size;
-	_ArrayLocaton = malloc(sizeof(T) * _ArraySize);
-	
+	_ArrayLocaton = malloc(sizeof(_T) * _ArraySize);
 }
 
-template<class T>
-inline void arr<T>::print()
+template<class _T>
+inline _T& arr<_T>::operator[](size_t i)
 {
-	printf("%i -> %i -> loc: %i", _ElemSize, sizeof(T) * _ArraySize, _ArrayLocaton);
+	assert(i < _ArraySize);
+	return (_T&)*((char*)_ArrayLocaton + (sizeof(_T) * i));
 }
-// IMPL
-
-
-
-
-
-
-
 
 
 #endif /* !ARRAYIMPL_H */
-
 
