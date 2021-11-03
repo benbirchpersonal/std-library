0a7a29a507c013f0b9c8c4db6178c6bb4187bb1a
benbirchpersonal <93481691+benbirchpersonal@users.noreply.github.com>
2021-11-03 09:49:32 +0000

    Update array_impl.h

--- array_impl.h
+++ array_impl.h
@@ -5,7 +5,7 @@
 #include <memory>
 #include <stdio.h>
 #include <cassert>
-
+#include "string-impl.h"
 constexpr size_t MAX_ARRAYSIZE = 100;
 static char ARRAY_OPERATION_BUFFER[MAX_ARRAYSIZE];
 
@@ -18,12 +18,27 @@ public:
 	* defualt constructor, type specified in template argument list <here>()
 	*/
 	arr(size_t size);
+	~arr();
 	// operators
 
 	/*
 	* index access
 	*/
 	_T& operator[](size_t i);
+
+	// getters
+
+	size_t count();
+
+	_T& first();
+	_T& last();
+
+
+
+	int findFirstOf(_T search);
+	int findLastOf(_T search);
+
+	void swap(size_t index1, size_t index2);
 
 private:
 	size_t _ElemSize;
@@ -45,6 +60,16 @@ inline arr<_T>::arr(size_t size)
 	_ArrayLocaton = malloc(sizeof(_T) * _ArraySize);
 }
 
+template<class T>
+inline arr<T>::~arr()
+{
+	free(_ArrayLocaton);
+}
+
+
+
+
+
 template<class _T>
 inline _T& arr<_T>::operator[](size_t i)
 {
@@ -53,5 +78,91 @@ inline _T& arr<_T>::operator[](size_t i)
 }
 
 
-#endif /* !ARRAYIMPL_H */
+template<class _T>
+inline size_t arr<_T>::count()
+{
+	return _ArraySize;
+}
 
+template<class _T>
+inline _T& arr<_T>::first()
+{
+	return (_T&)*((char*)_ArrayLocaton);
+}
+
+
+template<class _T>
+inline _T& arr<_T>::last()
+{
+	return (_T&)*((char*)_ArrayLocaton + (sizeof(_T) * (_ArraySize - 1)));
+}
+
+
+
+/*
+* Returns the index of the first occurance of a value in the array
+* returns -1 if no occurances of item
+* @param searchTerm
+*/
+template<class _T>
+inline int arr<_T>::findFirstOf(_T search)
+{
+	for (size_t i = 0; i < _ArraySize; i++)
+	{
+		_T curr = *((char*)_ArrayLocaton + (sizeof(_T) * (i)));
+		if (search == curr)
+			return i;
+	}
+
+	return -1;
+}
+
+
+/*
+* Returns the index of the last occurance of a value in the array
+* returns -1 if no occurances of item
+* @param searchTerm
+*/
+template<class _T>
+inline int arr<_T>::findLastOf(_T search)
+{
+	for (size_t i = _ArraySize - 1; i >= 0; i--)
+	{
+		_T curr = *((char*)_ArrayLocaton + (sizeof(_T) * (i)));
+		if (search == curr)
+			return i;
+	}
+	return -1;
+}
+template<class _T>
+void swapPointers(_T* p1, _T* p2) {
+	_T* temp = malloc(sizeof(_T));
+	memcpy(temp, p1, sizeof(_T));
+	memcpy(p1, p2, sizeof(_T));
+	memcpy(p2, temp, sizeof(_T));
+}
+
+
+
+
+
+template<class _T>
+inline void arr<_T>::swap(size_t index1, size_t index2)
+{
+	_T* loc1 = (_T*)((char*)_ArrayLocaton + (sizeof(_T) * index1));
+	_T* loc2 = (_T*)((char*)_ArrayLocaton + (sizeof(_T) * index2));
+	_T* temp = reinterpret_cast<_T*>(malloc(sizeof(_T)));
+	memcpy(temp, loc1, sizeof(_T));
+	memcpy(loc1, loc2, sizeof(_T));
+	memcpy(loc2, temp, sizeof(_T));
+	free(temp);
+
+}
+
+
+
+
+
+
+
+#endif /* !ARRAYIMPL_H */
