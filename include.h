486c64b283875bf6c14cc6c2295eb25a8d254c05
benbirchpersonal <93481691+benbirchpersonal@users.noreply.github.com>
2021-11-07 14:09:10 +0000

    Update include.h

--- include.h
+++ include.h
@@ -1,16 +1,37 @@
 #pragma once
-#define STRING
-#define ARRAY
-#define DYNAMIC_ARRAY
-
 #ifdef ARRAY
 #include "array-impl.h"
+#endif
+
+#ifdef STRING
+#include "string-impl.h"
 #endif
 
 #ifdef DYNAMIC_ARRAY
 #include "dynamic-array.h"
 #endif
 
-#ifdef STRING
-#include "string-impl.h"
-#endif
+
+/*
+CONVENTIONS
+
+class member function declarations:
+
+	
+KEYWORD		RETURN_TYPE		NAME	ARGS	KEYWORDS
+
+e.g
+
+_NODISCARD	size_t			count()			noexcept;
+			void			test()			noexcept;
+
+class member variable declarations:
+
+TYPE		NAME;
+
+e.g
+
+size_t		size;
+
+
+*/
