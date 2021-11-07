69107f4d7add236d4583148639d194909aa6dada
benbirchpersonal <93481691+benbirchpersonal@users.noreply.github.com>
2021-11-07 22:58:21 +0000

    Update example.cpp

--- example.cpp
+++ example.cpp
@@ -2,8 +2,8 @@
 #include "array-impl.h"
 #include "dynamic-array.h"
 int main() {
-    arr<int> x = {0,1,4,2,6,3,5,7,8,3,1,4,5,2,3,5,2,3,5,2};
-    dynArr<size_t> xy = x.find(2);
-    for (size_t s : xy)
-        printf("%i ", s);
+	arr<int> x = {0,1,4,2,6,3,5,7,8,3,1,4,5,2,3,5,2,3,5,2};
+	dynArr<size_t> xy = x.find(2);
+	for (size_t s : xy)
+		printf("%i ", s);
 }
