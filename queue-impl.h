8dce15220965f61b7377fd4f0998a8005c72ef21
benbirchpersonal <93481691+benbirchpersonal@users.noreply.github.com>
2021-11-08 12:15:12 +0000

    Update queue-impl.h

--- queue-impl.h
+++ queue-impl.h
@@ -1,23 +1,57 @@
-#pragma once
-
+#ifndef	QUEUE_IMPL
+#define	QUEUE_IMPL
 
 #include "include.h"
+
+#define DEFAULT_QUEUE_SIZE 100
+
 template <class _ElemType>
 class queue : public arr<_ElemType> {
 public:
+					queue<_ElemType>();
+
 			bool		enqueue(_ElemType item)			noexcept;
 			_ElemType	dequeue()				noexcept;
+	override	_ElemType	operaror[](size_t i)			noexcept;
 private:
-	
+			void		reallocate();
+private:
+	_ElemType*	_queueHead;
+	_ElemType*	_queueTail;
+
 };
 
 
-template <class _ElemType>
-bool queue<_ElemType>::enqueue(_ElemType elem) noexcept{
+template<class _ElemType>
+inline queue<_ElemType>::queue()
+{
+	this->_ElemSize			=	sizeof(_ElemType);
+	this->_ArrayLocation		=	malloc(this->_ElemSize * DEFAULT_QUEUE_SIZE);
+	this->_queueHead		=	this->_ArrayLocation   + DEFAULT_QUEUE_SIZE;
+	this->_queueTail		=	this->_queueHead;
+	this->_ArraySize		=	0;
+}
 
+
+template <class _ElemType>
+bool queue<_ElemType>::enqueue(_ElemType elem) noexcept {
+	if(this->queueTail)
 }
 
 template <class _ElemType>
 _ElemType queue<_ElemType>::dequeue() noexcept {
 
 }
+
+#endif /*QUEUE_IMPL*/
+
+
+
+/*
+
+	ab*?
+	a{5}(b|c)+
+
+
+
+*/
