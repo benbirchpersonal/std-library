ce9c6ee8bcc7b594686aa0f64b92521e6959b550
benbirchpersonal <93481691+benbirchpersonal@users.noreply.github.com>
2021-11-08 08:40:08 +0000

    Update example.cpp

--- example.cpp
+++ example.cpp
@@ -1,9 +1,13 @@
 #include "string-impl.h"
 #include "array-impl.h"
-#include "dynamic-array.h"
+#include "stack-impl.h"
 int main() {
 	arr<int> x = {0,1,4,2,6,3,5,7,8,3,1,4,5,2,3,5,2,3,5,2};
-	dynArr<size_t> xy = x.find(2);
+	stack<size_t> xy = x.find(2);
 	for (size_t s : xy)
 		printf("%i ", s);
+
+	str teststr = "";
+	teststr += "test";
+
 }
