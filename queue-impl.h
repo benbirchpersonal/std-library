9c2e8955480ef7f457d84d3e06939343b64ff6d7
benbirchpersonal <93481691+benbirchpersonal@users.noreply.github.com>
2021-12-15 15:53:59 +0000

    Update queue-impl.h

--- queue-impl.h
+++ queue-impl.h
@@ -8,13 +8,13 @@
 template <class _ElemType>
 class queue : public arr<_ElemType> {
 public:
-					queue<_ElemType>();
+							queue<_ElemType>();
 
-			bool		enqueue(_ElemType item)			noexcept;
-			_ElemType	dequeue()				noexcept;
-	override	_ElemType	operaror[](size_t i)			noexcept;
+				bool		enqueue(_ElemType item)			noexcept;
+				_ElemType	dequeue()						noexcept;
+	override	_ElemType	operator[](size_t i)			noexcept;
 private:
-			void		reallocate();
+	_NODISCARD	void		realloc();
 private:
 	_ElemType*	_queueHead;
 	_ElemType*	_queueTail;
@@ -26,23 +26,65 @@ template<class _ElemType>
 inline queue<_ElemType>::queue()
 {
 	this->_ElemSize			=	sizeof(_ElemType);
-	this->_ArrayLocation		=	malloc(this->_ElemSize * DEFAULT_QUEUE_SIZE);
-	this->_queueHead		=	this->_ArrayLocation   + DEFAULT_QUEUE_SIZE;
+	this->_ArrayLocation	=	malloc(this->_ElemSize * DEFAULT_QUEUE_SIZE);
+	this->_queueHead		=	(_ElemType*)this->_ArrayLocation  + (_ElemType*)DEFAULT_QUEUE_SIZE - 1;
 	this->_queueTail		=	this->_queueHead;
 	this->_ArraySize		=	0;
 }
 
 
 template <class _ElemType>
-bool queue<_ElemType>::enqueue(_ElemType elem) noexcept {
+bool queue<_ElemType>::enqueue(const _ElemType& elem) noexcept {
+	this->_queueTail--;
+	this->_ArraySize++;
+	if(this->_queueTail == this->_ArrayLocation)
+		assert(realloc()); // Allocation failed, overflow? not enough heap memory.
+	memcpy_s(
+		this->_ArrayLocation + this->_queueTail,
+		&elem,
+		_ElemSize,
+		_ElemSize
+		);
+}
+
+template<class _ElemType>
+_NODISCARD inline bool stack<_ElemType>::realloc()
+{
+	_ElemType* relativeTail =	this->_queueTail - this->_ArrayLocation;
+	_ElemType* relativeHead =	this->_queueHead - this->_ArrayLocation;
+
+	_ElemType* newLocation	=	reinterpret_cast<_ElemType*>(malloc(sizeof(_ElemType) * this->_ArrayAvailableSize * 2));
+	if (newLocation == nullptr)
+	return false;
+
+	memcpy_s(
+		newLocation,
+		sizeof(_ElemType) * this->_ArrayAvailableSize * 2,
+		this->_ArrayLocation,
+		sizeof(_ElemType) * this->_ArraySize
+	);
+
+	free(this->_ArrayLocation);
+
+	this->_ArrayLocation		=	newLocation;
+	this->_ArrayAvailableSize	*=	2;
+
+	this->_queueHead 			= this->_ArrayLocation + relativeHead;
+	this->_queueTail 			= this->_ArrayLocation + relativeTail;
+
+	return true;
 }
 
 template <class _ElemType>
 _ElemType queue<_ElemType>::dequeue() noexcept {
-	assert(this->_ArraySize > 0); 								// Empty Queue
+	assert(this->_ArraySize > 0);
+	assert(this->_queueHead != this->_queueTail);
+
+	free(this->_queueHead);							// Empty Queue
+	this->_queueHead--;
+	this->_ArraySize--;
 }
 
 #endif /*QUEUE_IMPL*/
-
 
 
