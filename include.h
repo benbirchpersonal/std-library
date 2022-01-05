1677434240bb0dac95917b90c9632ced432114e6
benbirchpersonal <93481691+benbirchpersonal@users.noreply.github.com>
2022-01-05 16:52:30 +0000

    Update include.h

--- include.h
+++ include.h
@@ -1,9 +1,11 @@
 #pragma once
-
+#ifndef INCLUDED
+#define INCLUDED
 #include <cassert>
 #include <memory>
 
 class str;
+typedef str string;
 
 template<class _ElemType>
 class arr;
@@ -12,7 +14,23 @@ template<class _ElemType>
 class stack;
 
 template<class _ElemType>
+using dynamicArray = stack<_ElemType>
+
+template<class _ElemType>
+using list = stack<_ElemType>
+
+
+template<class _ElemType>
 class queue;
+
+template<class _ElemType>
+class circularQueue;
+
+#include "array-impl.h"
+#include "stack-impl.h"
+#include "queue-impl.h"
+#include "circular-queue-impl.h"
+#include "string-impl.h"
 
 
 /*
@@ -20,13 +38,13 @@ CONVENTIONS
 
 class member function declarations:
 
-    
+
 KEYWORD		RETURN_ElemTypeYPE		NAME	ARGS	KEYWORDS
 
 e.g
 
 _NODISCARD	size_t			count()			noexcept;
-            void			test()			noexcept;
+			void			test()			noexcept;
 
 class member variable declarations:
 
@@ -38,3 +56,6 @@ size_t		size;
 
 
 */
+
+
+#endif
