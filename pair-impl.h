9300d2c4af96dd4571e938dbff10120974cc98d0
benbirchpersonal <93481691+benbirchpersonal@users.noreply.github.com>
2022-01-19 11:53:00 +0000

    Update pair-impl.h

--- pair-impl.h
+++ pair-impl.h
@@ -9,10 +9,13 @@ public:
 	pair<_ElemType>(const std::initializer_list<_ElemType>&);
 	pair<_ElemType>(const _ElemType&, const _ElemType&);
 	~pair();
-
+	
+	const void	 operator=(const pair<_ElemType>& copy);
 	const _ElemType& operator[](size_t i);
 	const _ElemType& first();
 	const _ElemType& second();
+	
+	void swap() noexcept;
 
 
 
@@ -85,17 +88,69 @@ pair::~pair(){
 
 }
 
-
-const _ElemType& operator[](size_t i){
+template <class _ElemType>
+const _ElemType& pair::operator[](size_t i){
 	assert(this->_first != nullptr);
 	assert(i > 2);
 	return *(this->_first + i);
 }
 
-const _ElemType& first() {
+template <class _ElemType>
+const _ElemType& pair::first() {
 	return this->_first;
 }
 
-const _ElemType& second() {
+template <class _ElemType>
+const _ElemType& pair::second() {
 	return this->_second;
 }
+
+
+template <class _ElemType>
+const void pair::operator=(const pair<_ElemType>& copy) {
+	memcpy_s(
+		this,
+		sizeof(this),
+		copy,
+		sizeof(this)
+	);
+}
+
+
+
+
+template <class _ElemType>
+void pair::swap() {
+	_ElemType* temp = (_ElemType*) malloc(sizeof(_ElemType));
+	assert(this->_first != nullptr);
+	assert(this->_second != nullptr);
+	memcpy_s(
+		temp, 
+		sizeof(_ElemType),
+		this->_first,
+		sizeof(_ElemType)
+	);
+	
+	memcpy_s(
+		this->_first, 
+		sizeof(_ElemType),
+		this->_second,
+		sizeof(_ElemType)
+	);
+	
+	memcpy_s(
+		this->_second, 
+		sizeof(_ElemType),
+		temp,
+		sizeof(_ElemType)
+	);
+	
+	
+	
+	
+}
+
+
+
+
+
