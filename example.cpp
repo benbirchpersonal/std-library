56bf4e4525d6f1d878687a26f1e79ed5d2089379
benbirchpersonal <93481691+benbirchpersonal@users.noreply.github.com>
2022-01-21 07:30:30 +0000

    Update example.cpp

--- example.cpp
+++ example.cpp
@@ -1,13 +1,6 @@
-#include "string-impl.h"
 #include "array-impl.h"
-#include "stack-impl.h"
+#include "initializer-list.h"
+
 int main() {
-	arr<int> x = {0,1,4,2,6,3,5,7,8,3,1,4,5,2,3,5,2,3,5,2};
-	stack<size_t> xy = x.find(2);
-	for (size_t s : xy)
-		printf("%i ", s);
-
-	str teststr = "";
-	teststr += "test";
-
+	snd::arr<int> x ( (initList<int>)((1, 2,2,2,43,2,234,2,343,4) ));
 }
