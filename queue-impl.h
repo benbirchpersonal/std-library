2fc679f913637436b26dbd5ae95f779388dffb45
benbirchpersonal <93481691+benbirchpersonal@users.noreply.github.com>
2022-01-24 10:49:53 +0000

    Update queue-impl.h

--- queue-impl.h
+++ queue-impl.h
@@ -1,26 +1,34 @@
 #ifndef	QUEUE_IMPL
 #define	QUEUE_IMPL
 
-#include "include.h"
+#include "helpers.h"
 #define DEFAULT_QUEUE_SIZE 100
 
 _STDLIB_BEGIN
 template <class _ElemType>
-class queue : public stack<_ElemType>
+class queue : public arr<_ElemType>
 {
 public:
-	queue<_ElemType> ();
-	queue<_ElemType> ( std::initializer_list<_ElemType> initList );
+	queue<_ElemType>();
 
-	bool		enqueue ( const _ElemType& item )				noexcept;
-	_ElemType	dequeue ()									noexcept;
-	_ElemType	operator[]( size_t i )						noexcept;
+	bool		enqueue(const _ElemType& item)				noexcept;
+	_ElemType	dequeue()									noexcept;
+	_ElemType	operator[](size_t i)						noexcept;
+	void		rotate()									noexcept;
+
+	using		iterator = _ElemType*;
+	_NODISCARD	iterator		begin()	const noexcept {
+		return _QueueTail;
+	}
+	_NODISCARD	iterator		end()	const noexcept {
+		return _QueueHead + 1;
+	}
 
 protected:
 
 
-	_NODISCARD  void		realloc ();
-	bool		reserve ( size_t newSize );
+	_NODISCARD  void		realloc();
+				bool		reserve(size_t newSize);
 
 	_ElemType* _QueueHead;
 	_ElemType* _QueueTail;
@@ -29,60 +37,85 @@ protected:
 
 	size_t _ArrayAllocatedSize;
 
+
+public:			// INITIALIZER LIST CONSTRUCTORS
+
+	template <typename... UList, REQUIRES(nonarrow_convertible<_ElemType, UList...>::value)>
+	queue(UList &&... vs)
+	{
+		this->_ElemSize = sizeof(_ElemType);
+		this->_ArraySize = 0;
+		this->_ArrayLocation = nullptr;
+		process(forward<UList>(vs)...);
+
+		this->_ArrayLocation = (_ElemType*) malloc(sizeof(_ElemType) * this->_ArraySize * 2 );
+
+		this->_ArrayUpperBound = this->_ArrayLocation + (this->_ArraySize * 2);
+
+		this->_ArrayAllocatedSize = (sizeof(_ElemType) * this->_ArraySize * 2);
+
+		this->_QueueHead = this->_ArrayLocation + ((this->_ArraySize * 2) / 2);
+		this->_QueueTail = _QueueHead;
+		this->_ArraySize = 0;
+
+		processSecondPass(forward<UList>(vs)...);
+	}
+
+
+private:
+
+	template <typename U, typename... UList>
+	void process(U&& v, UList &&... vs)
+	{
+		this->_ArraySize++;
+		process(forward<UList>(vs)...);
+	}
+
+	void process(){}
+
+	template <typename U, typename... UList>
+	void processSecondPass(U&& v, UList &&... vs)
+	{
+		auto x = forward<U>(v);
+		this->enqueue(forward<U>(v));
+
+		processSecondPass(forward<UList>(vs)...);
+	}
+	void processSecondPass() {}
 };
 
 
 
-
 template<class _ElemType>
-inline queue<_ElemType>::queue ()
+inline queue<_ElemType>::queue()
 {
-	this->_ArrayLocation = ( _ElemType* ) malloc ( sizeof ( _ElemType ) * DEFAULT_QUEUE_SIZE );
+	this->_ArrayLocation = (_ElemType*)malloc(sizeof(_ElemType) * DEFAULT_QUEUE_SIZE);
 
 	this->_ArrayUpperBound = this->_ArrayLocation + DEFAULT_QUEUE_SIZE;
 
 	this->_ArraySize = 0;
 	this->_ArrayAllocatedSize = DEFAULT_QUEUE_SIZE;
 
-	this->_QueueHead = this->_ArrayLocation + ( DEFAULT_QUEUE_SIZE / 2 );
+	this->_QueueHead = this->_ArrayLocation + (DEFAULT_QUEUE_SIZE / 2);
 	this->_QueueTail = _QueueHead;
 
 }
 
-template<class _ElemType>
-inline queue<_ElemType>::queue ( std::initializer_list<_ElemType> initList )
-{
-	this->_ArrayLocation = ( _ElemType* ) malloc ( sizeof ( _ElemType ) * ( DEFAULT_QUEUE_SIZE > initList.size () ? DEFAULT_QUEUE_SIZE : initList.size () * 2 ) );
-
-	this->_ArrayUpperBound = this->_ArrayLocation + ( DEFAULT_QUEUE_SIZE > initList.size () ? DEFAULT_QUEUE_SIZE : initList.size () * 2 );
-
-	this->_ArraySize = 0;
-	this->_ArrayAllocatedSize = ( DEFAULT_QUEUE_SIZE > initList.size () ? DEFAULT_QUEUE_SIZE : initList.size () * 2 );
-
-	this->_QueueHead = this->_ArrayLocation + ( ( DEFAULT_QUEUE_SIZE > initList.size () ? DEFAULT_QUEUE_SIZE : initList.size () * 2 ) / 2 );
-	this->_QueueTail = _QueueHead;
-
-	for ( const _ElemType& x : initList )
-	{
-		this->enqueue ( x );
-	}
-}
-
 
 template<class _ElemType>
-inline bool queue<_ElemType>::enqueue ( const _ElemType& item ) noexcept
+inline bool queue<_ElemType>::enqueue(const _ElemType& item) noexcept
 {
-	if ( this->_ArraySize == 0 )
+	if (this->_ArraySize == 0)
 	{
-		*this->_QueueTail = _ElemType ( item );
+		*this->_QueueTail = _ElemType(item);
 		this->_ArraySize = 1;
 		//this->_QueueTail--;
 		return true;
 	}
 
 
-	if ( ( this->_QueueTail - 1 ) <= this->_ArrayLocation )
-		realloc ();
+	if ((this->_QueueTail - 1) <= this->_ArrayLocation)
+		realloc();
 
 	this->_ArraySize++;
 	this->_QueueTail--;
@@ -94,75 +127,75 @@ inline bool queue<_ElemType>::enqueue ( const _ElemType& item ) noexcept
 		this->_ElemSize
 	);
 	*/
-	*this->_QueueTail = _ElemType ( item );
+	*this->_QueueTail = _ElemType(item);
 	return true;
 
 }
 
 template<class _ElemType>
-inline _ElemType queue<_ElemType>::dequeue () noexcept
+inline _ElemType queue<_ElemType>::dequeue() noexcept
 {
-	assert ( this->_ArraySize > 0 );
+	assert(this->_ArraySize > 0);
 	this->_QueueHead--;
 	this->_ArraySize--;
-	return *( this->_QueueHead + 1 );
+	return *(this->_QueueHead + 1);
 }
 
 template<class _ElemType>
-inline _ElemType queue<_ElemType>::operator[]( size_t i ) noexcept
+inline _ElemType queue<_ElemType>::operator[](size_t i) noexcept
 {
-	//assert((this->_QueueHead - i) >= this->_ArrayLocation);
-	return *( this->_QueueHead - i );
+	assert(i < this->_ArraySize);
+	return *(this->begin() + i);
 }
 
 template<class _ElemType>
-inline void queue<_ElemType>::realloc ()
+inline void queue<_ElemType>::realloc()
 {
-	if ( this->_ArraySize >= this->_ArrayAllocatedSize )
+	if (this->_ArraySize >= this->_ArrayAllocatedSize)
 	{
-		_ElemType* tempArrayLocation = ( _ElemType* ) malloc ( sizeof ( _ElemType ) * this->_ArrayAllocatedSize * 2 );
+		_ElemType* tempArrayLocation = (_ElemType*)malloc(sizeof(_ElemType) * this->_ArrayAllocatedSize * 2);
 #ifdef DEBUG
-		memset ( tempArrayLocation , 0 , sizeof ( _ElemType ) * this->_ArrayAllocatedSize * 2 );
+		memset(tempArrayLocation, 0, sizeof(_ElemType) * this->_ArrayAllocatedSize * 2);
 #endif
-		size_t centerDistanceFromTail = ( this->_ArrayLocation + ( this->_ArrayAllocatedSize / 2 ) ) - this->_QueueTail;
+		size_t centerDistanceFromTail = (this->_ArrayLocation + (this->_ArrayAllocatedSize / 2)) - this->_QueueTail;
 
-		memcpy_s (
-			tempArrayLocation + this->_ArrayAllocatedSize - centerDistanceFromTail ,
-			this->_ArraySize * sizeof ( _ElemType* ) ,
-			this->_QueueTail ,
-			this->_ArraySize * sizeof ( _ElemType* )
+		memcpy_s(
+			tempArrayLocation + this->_ArrayAllocatedSize - centerDistanceFromTail,
+			this->_ArraySize * sizeof(_ElemType*),
+			this->_QueueTail,
+			this->_ArraySize * sizeof(_ElemType*)
 		);
 		this->_ArrayAllocatedSize *= 2;
-		this->_ArrayUpperBound = tempArrayLocation + ( this->_ArrayAllocatedSize );
+		this->_ArrayUpperBound = tempArrayLocation + (this->_ArrayAllocatedSize);
 
-		this->_QueueTail = tempArrayLocation + ( this->_ArrayAllocatedSize / 2 ) - centerDistanceFromTail;
+		this->_QueueTail = tempArrayLocation + (this->_ArrayAllocatedSize / 2) - centerDistanceFromTail;
 		this->_QueueHead = this->_QueueTail + this->_ArraySize - 1;
 
-		free ( this->_ArrayLocation );
+		free(this->_ArrayLocation);
 		this->_ArrayLocation = tempArrayLocation;
 	}
 
 	else
 	{
-		_ElemType* tempArrayLocation = ( _ElemType* ) malloc ( sizeof ( _ElemType ) * this->_ArrayAllocatedSize );
+		_ElemType* tempArrayLocation = (_ElemType*)malloc(sizeof(_ElemType) * this->_ArrayAllocatedSize);
 #ifdef DEBUG
-		memset ( tempArrayLocation , 0 , sizeof ( _ElemType ) * this->_ArrayAllocatedSize );
+		memset(tempArrayLocation, 0, sizeof(_ElemType) * this->_ArrayAllocatedSize);
 #endif
-		size_t centerDistanceFromTail = ( this->_ArrayLocation + ( this->_ArrayAllocatedSize / 2 ) ) - this->_QueueTail;
+		size_t centerDistanceFromTail = (this->_ArrayLocation + (this->_ArrayAllocatedSize / 2)) - this->_QueueTail;
 
-		memcpy_s (
-			tempArrayLocation + ( this->_ArrayAllocatedSize ) - centerDistanceFromTail ,
-			this->_ArraySize * sizeof ( _ElemType* ) ,
-			this->_QueueTail ,
-			this->_ArraySize * sizeof ( _ElemType* )
+		memcpy_s(
+			tempArrayLocation + (this->_ArrayAllocatedSize) - centerDistanceFromTail,
+			this->_ArraySize * sizeof(_ElemType*),
+			this->_QueueTail,
+			this->_ArraySize * sizeof(_ElemType*)
 		);
 
-		this->_ArrayUpperBound = tempArrayLocation + ( this->_ArrayAllocatedSize );
+		this->_ArrayUpperBound = tempArrayLocation + (this->_ArrayAllocatedSize);
 
-		this->_QueueTail = tempArrayLocation + ( this->_ArrayAllocatedSize ) - centerDistanceFromTail;
+		this->_QueueTail = tempArrayLocation + (this->_ArrayAllocatedSize) - centerDistanceFromTail;
 		this->_QueueHead = this->_QueueTail + this->_ArraySize - 1;
 
-		free ( this->_ArrayLocation );
+		free(this->_ArrayLocation);
 		this->_ArrayLocation = tempArrayLocation;
 	}
 
@@ -170,29 +203,35 @@ inline void queue<_ElemType>::realloc ()
 }
 
 template<class _ElemType>
-inline bool queue<_ElemType>::reserve ( size_t newSize )
-{
-	assert ( newSize > this->_ArrayAllocatedSize );
-	_ElemType* tempArrayLocation = ( _ElemType* ) malloc ( sizeof ( _ElemType ) * newSize );
-#ifdef DEBUG
-	memset ( tempArrayLocation , 0 , sizeof ( _ElemType ) * this->_ArrayAllocatedSize * 2 );
-#endif
-	size_t centerDistanceFromTail = ( this->_ArrayLocation + ( this->_ArrayAllocatedSize / 2 ) ) - this->_QueueTail;
+inline void queue<_ElemType>::rotate() noexcept{
+	this->enqueue(this->dequeue());
+}
 
-	memcpy_s (
-		tempArrayLocation + this->_ArrayAllocatedSize - centerDistanceFromTail ,
-		this->_ArraySize * sizeof ( _ElemType* ) ,
-		this->_QueueTail ,
-		this->_ArraySize * sizeof ( _ElemType* )
+template<class _ElemType>
+inline bool queue<_ElemType>::reserve(size_t newSize)
+{
+	assert(newSize > this->_ArrayAllocatedSize);
+	_ElemType* tempArrayLocation = (_ElemType*)malloc(sizeof(_ElemType) * newSize);
+#ifdef DEBUG
+	memset(tempArrayLocation, 0, sizeof(_ElemType) * this->_ArrayAllocatedSize * 2);
+#endif
+	size_t centerDistanceFromTail = (this->_ArrayLocation + (this->_ArrayAllocatedSize / 2)) - this->_QueueTail;
+
+	memcpy_s(
+		tempArrayLocation + this->_ArrayAllocatedSize - centerDistanceFromTail,
+		this->_ArraySize * sizeof(_ElemType*),
+		this->_QueueTail,
+		this->_ArraySize * sizeof(_ElemType*)
 	);
 	this->_ArrayAllocatedSize = newSize;
-	this->_ArrayUpperBound = tempArrayLocation + ( this->_ArrayAllocatedSize );
+	this->_ArrayUpperBound = tempArrayLocation + (this->_ArrayAllocatedSize);
 
-	this->_QueueTail = tempArrayLocation + ( this->_ArrayAllocatedSize / 2 ) - centerDistanceFromTail;
+	this->_QueueTail = tempArrayLocation + (this->_ArrayAllocatedSize / 2) - centerDistanceFromTail;
 	this->_QueueHead = this->_QueueTail + this->_ArraySize - 1;
 
-	free ( this->_ArrayLocation );
+	free(this->_ArrayLocation);
 	this->_ArrayLocation = tempArrayLocation;
+	return true;
 }
 
 
