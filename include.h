16d4308d6e33b645440ec069df34a412f48d55f3
benbirchpersonal <93481691+benbirchpersonal@users.noreply.github.com>
2022-01-24 10:48:49 +0000

    Update include.h

--- include.h
+++ include.h
@@ -1,51 +1,10 @@
-#pragma once
-#include <memory>
-#include <assert.h>
-#ifdef DEBUG
-extern void __assert ( const char* msg , const char* file , int line );
-#define assert(EX) (void)((EX) || (__assert (#EX, __FILE__, __LINE__),0))
-#else
-#define assert(EX) (void)(EX) ;
-#endif
-
-
-#define _STDLIB_BEGIN namespace snd {
-#define _STDLIB_END };
-template <typename T>
-class initList;
-
-_STDLIB_BEGIN
-
-class str;
-typedef str string;
-
-template<class _ElemType>
-class arr;
-
-template<class _ElemType>
-class stack;
-
-template<class _ElemType>
-using dynamicArray = stack<_ElemType>;
-
-template<class _ElemType>
-using list = stack<_ElemType>;
-
-
-template<class _ElemType>
-class queue;
-
-template<class _ElemType>
-class circularQueue;
-
-
-_STDLIB_END
-
-
+#ifndef INCLUDED
+#define INCLUDED
 
 #include "array-impl.h"
 #include "stack-impl.h"
 #include "queue-impl.h"
 #include "circular-queue-impl.h"
 #include "string-impl.h"
-#include "initializer-list.h"
+
+#endif // INCLUDED
