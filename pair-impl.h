ceb81014437903fd19c9f381effa33660c4db125
benbirchpersonal <93481691+benbirchpersonal@users.noreply.github.com>
2022-01-24 10:49:39 +0000

    Update pair-impl.h

--- pair-impl.h
+++ pair-impl.h
@@ -1,30 +1,37 @@
-#include <memory>
-#include <initializer_list>
+#ifndef PAIR_INCLUDED
+#define PAIR_INCLUDED
 
+#include "helpers.h"
 
+_STDLIB_BEGIN
 template <class _ElemType1, class _ElemType2> 
 class pair {
 public:
-	pair<_ElemType>();
-	pair<_ElemType>(const std::initializer_list<_ElemType1, _ElemType2>&);
-	pair<_ElemType>(const _ElemType&, const _ElemType2&);
+	pair();
+	pair(const _ElemType1&, const _ElemType2&);
+	pair(std::initializer_list<_ElemType1> initList);
 	~pair();
-	
+
 	const void	 operator=(const pair<_ElemType1, _ElemType2>& copy);
 	const _ElemType1& first();
 	const _ElemType2& second();
-	
-	void swap() noexcept;
+
+	pair<_ElemType1, _ElemType2> swap() noexcept;
 
 
 
 private:
-	_ElemType* _first;
-	_ElemType* _second;
-}
+	_ElemType1* _first;
+	_ElemType2* _second;
+};
+
+
+
+
+
 
 template <class _ElemType1, class _ElemType2> 
-pair::pair(){
+pair<_ElemType1, _ElemType2>::pair(){
 	this->_first = (_ElemType1*)malloc(sizeof(_ElemType1) + sizeof(_ElemType2));
 	this->_second = (_ElemType2)(this->_first + 1);
 
@@ -35,38 +42,20 @@ pair::pair(){
 }
 
 template <class _ElemType1, class _ElemType2> 
-pair::pair(const std::initializer_list<_ElemType1, _ElemType2>& initList)  {
-	assert(initList.size() == 2);
+pair<_ElemType1 ,_ElemType2>::pair(const _ElemType1& obj1, const _ElemType2& obj2)  {
 	this->_first = (_ElemType1*)malloc(sizeof(_ElemType1) + sizeof(_ElemType2));
-	this->second = this->_first + 1;
-	memcpy_s(
-		(void*) 		this->_first,
-					sizeof(_ElemType1) + sizeof(_ElemType2),
-		(const void*)	        &initList,
-					sizeof(_ElemType1) + sizeof(_ElemType2)
-	);
-
-	#ifdef DEBUG
-		printf("copied initializer_list to %p", this->_first);
-	#endif
-}
-
-template <class _ElemType1, class _ElemType2> 
-pair::pair((const _ElemType1& obj1, const _ElemType2& obj2)  {
-	assert(initList.size() == 2);
-	this->_first = (_ElemType1*)malloc(sizeof(_ElemType1) + sizeof(_ElemType2));
-	this->_second = (_ElemType2*) (this->_first + 1;)
+	this->_second = (_ElemType2*)(this->_first + 1);
 	memcpy_s(
 		(void*) 		this->_first,
 					sizeof(_ElemType1),
-		(const void*)	        obj1,
+		(const void*)	        &obj1,
 					sizeof(_ElemType1)
 	);
 
 	memcpy_s(
 		(void*) 		this->_second,
 					sizeof(_ElemType2),
-		(const void*)	        obj2,
+		(const void*)	        &obj2,
 					sizeof(_ElemType2)
 	);
 
@@ -75,29 +64,53 @@ pair::pair((const _ElemType1& obj1, const _ElemType2& obj2)  {
 	#endif
 }
 
+template <class _ElemType1, class _ElemType2>
+pair<_ElemType1, _ElemType2>::pair(std::initializer_list<_ElemType1> initList) {
+	this->_first = (_ElemType1*)malloc(sizeof(_ElemType1) + sizeof(_ElemType2));
+	this->_second = (_ElemType2*)(this->_first + 1);
+	memcpy_s(
+		(void*)this->_first,
+		sizeof(_ElemType1),
+		initList.begin(),
+		sizeof(_ElemType1)
+	);
+
+	memcpy_s(
+		(void*)this->_second,
+		sizeof(_ElemType2),
+		initList.end(),
+		sizeof(_ElemType2)
+	);
+
+#ifdef DEBUG
+	printf("copied initializer_list to %p", this->_first);
+#endif
+}
+
+
+
+
+
 template <class _ElemType1, class _ElemType2> 
-pair::~pair(){
+pair<_ElemType1, _ElemType2>::~pair(){
 	#ifdef DEBUG
 		printf("out of scope at %p",this->_first );
 	#endif
 
-		if(this->first)
-			free(this->first);
-
 }
 
 template <class _ElemType1, class _ElemType2> 
-const _ElemType1& pair::first() {
-	return this->_first;
+const _ElemType1& pair<_ElemType1, _ElemType2>::first() {
+	return *this->_first;
 }
 template <class _ElemType1, class _ElemType2> 
-const _ElemType2& pair::second() {
-	return this->_second;
+const _ElemType2& pair<_ElemType1, _ElemType2>::second() {
+	return *this->_second;
 }
 
 
 template <class _ElemType1, class _ElemType2> 
-const void pair::operator=(const pair<_ElemType1, _ElemType2>& copy) {
+const void pair<_ElemType1, _ElemType2>::operator=(const pair<_ElemType1, _ElemType2>& copy) {
 	memcpy_s(
 		this,
 		sizeof(this),
@@ -110,11 +123,11 @@ const void pair::operator=(const pair<_ElemType1, _ElemType2>& copy) {
 
 
 template <class _ElemType1, class _ElemType2> 
-void pair::swap() {
+pair<_ElemType1, _ElemType2> pair<_ElemType1, _ElemType2>::swap() noexcept{
 	_ElemType1* temp = (_ElemType1*) malloc(sizeof(_ElemType1));
 	assert(this->_first != nullptr);
 	assert(this->_second != nullptr);
-	assert(typeof(_ElemType1) == typeof(_ElemType2))
+	assert(typeid(_ElemType1) == typeid(_ElemType2));
 	memcpy_s(
 		temp, 
 		sizeof(_ElemType1),
@@ -136,4 +149,11 @@ void pair::swap() {
 		sizeof(_ElemType1)
 	);
 
+	return *this;
+
 }
+
+_STDLIB_END
+
+
+#endif // PAIR_INCLUDED
