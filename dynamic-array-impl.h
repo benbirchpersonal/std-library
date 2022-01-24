45152e78ec2030093a7fdd1bd241ba4ce3767c86
benbirchpersonal <93481691+benbirchpersonal@users.noreply.github.com>
2022-01-24 12:12:33 +0000

    Update dynamic-array-impl.h

--- dynamic-array-impl.h
+++ dynamic-array-impl.h
@@ -7,10 +7,10 @@ template <class _ElemType>
 class dynamicArray : arr<_ElemType> 
 {
 			dynamicArray<_ElemType>();
-
 private:
 	_ElemType*	_DataTail;
 	_ElemType* 	_DataHead;
+	size_t 		_ArrayAllocatedSize;
 }
 
 
@@ -20,4 +20,8 @@ private:
 template<class _ElemType>
 dynamicArray<_ElemType>::dynamicArray(){
 	this->_DataTail = static_cast<_ElemType*>(malloc(sizeof(_ElemType) * DEFAULT_DYNAMIC_ARRAY_SIZE));
+	this->_DataHead = this->_DataTail + DEFAULT_DYNAMIC_ARRAY_SIZE;
+	this->_ArrayLocation = this->_DataTail + DEFAULT_DYNAMIC_ARRAY_SIZE + 1;
+	this->_ArraySize = 0;
+	this->_ArrayAllocatedSize = DEFAULT_DYNAMIC_ARRAY_SIZE;
 }
