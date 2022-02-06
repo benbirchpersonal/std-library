7e80d82837794b9e9ea4b9132077ca9a49b3fbb4
benbirchpersonal <93481691+benbirchpersonal@users.noreply.github.com>
2022-02-06 15:01:34 +0000

    Update circular-queue-impl.h

--- circular-queue-impl.h
+++ circular-queue-impl.h
@@ -1,77 +1,26 @@
 #ifndef	CIRCULAR_QUEUE_IMPL
 #define	CIRCULAR_QUEUE_IMPL
 
-#include "helpers.h"
+#include "include.h"
 #define DEBUG
-_STDLIB_BEGIN
-
 template <class _ElemType>
-class circularQueue : public arr<_ElemType>
-{
+class circularQueue : public arr<_ElemType> {
 public:
-	circularQueue<_ElemType>(size_t queueSize);
+				circularQueue<_ElemType>(size_t queueSize);
+				circularQueue<_ElemType>(std::initializer_list<_ElemType> initList);
 
 	bool		enqueue(const _ElemType& item)				noexcept;
 	_ElemType	dequeue()									noexcept;
 	_ElemType	operator[](size_t i)						noexcept;
-	void		rotate()									noexcept;
 
-
-
-	using		iterator = _ElemType*;
-	_NODISCARD	iterator		begin()	const noexcept {
-		return this->_ArrayLocation + _QueueHead;
-	}
-	_NODISCARD	iterator		end()	const noexcept {
-		return this->_ArrayLocation + _QueueTail + 1;
-	}
 public:
 
 
 	_NODISCARD  void		realloc();
-				bool		reserve(size_t newSize);
+	bool		reserve(size_t newSize);
 
 	int _QueueHead;
 	int _QueueTail;
-
-
-
-public:			// INITIALIZER LIST CONSTRUCTORS
-
-	template <typename... UList, REQUIRES(nonarrow_convertible<_ElemType, UList...>::value)>
-	circularQueue(UList &&... vs)
-	{
-		
-		this->_QueueHead = -1;
-		this->_QueueTail = -1;
-		process(forward<UList>(vs)...);
-		this->_ArrayLocation = (_ElemType*)malloc(sizeof(_ElemType) * this->_ArraySize * 2);
-		this->_ArraySize = 0;
-
-		processSecondPass(forward<UList>(vs)...);
-	}
-
-
-private:
-
-	template <typename U, typename... UList>
-	void process(U&& v, UList &&... vs)
-	{
-		this->_ArraySize++;
-		process(forward<UList>(vs)...);
-	}
-
-	void process() {}
-
-	template <typename U, typename... UList>
-	void processSecondPass(U&& v, UList &&... vs)
-	{
-		auto x = forward<U>(v);
-		this->enqueue(forward<U>(v));
-
-		processSecondPass(forward<UList>(vs)...);
-	}
-	void processSecondPass() {}
 
 
 };
@@ -89,15 +38,30 @@ inline circularQueue<_ElemType>::circularQueue(size_t queueSize)
 
 }
 
+template<class _ElemType>
+inline circularQueue<_ElemType>::circularQueue(std::initializer_list<_ElemType> initList)
+{
+
+	this->_ArrayLocation = (_ElemType*)malloc(sizeof(_ElemType) * initList.size());
+#ifdef DEBUG
+	memset(this->_ArrayLocation, 0, sizeof(_ElemType) * initList.size());
+#endif	
+	this->_ArraySize = initList.size();
+	this->_QueueHead = -1;
+	this->_QueueTail = -1;
+
+	for (const _ElemType& x : initList)
+		this->enqueue(x);
+
+}
 
 template<class _ElemType>
 inline bool circularQueue<_ElemType>::enqueue(const _ElemType& item) noexcept
 {
-	if (_QueueHead == _QueueTail && this->_ArraySize > 1)
+	if ((this->_QueueHead == 0 && this->_QueueTail == this->_ArraySize - 1) || (this->_QueueTail == (this->_QueueHead - 1) % (this->_ArraySize - 1)) )
 		return false;
 
-	else if (this->_QueueHead == -1)
-	{
+	else if (this->_QueueHead == -1) {
 		this->_QueueHead = 0;
 		this->_QueueTail = 0;
 		memcpy(
@@ -105,39 +69,28 @@ inline bool circularQueue<_ElemType>::enqueue(const _ElemType& item) noexcept
 			&item,
 			sizeof(item)
 		);
-		this->_ArraySize++;
 		return true;
 	}
 
-	else if ((this->_QueueTail == (this->_ArraySize - 1)) && (this->_QueueHead != 0))
-	{
+	else if ((this->_QueueTail == (this->_ArraySize - 1)) && (this->_QueueHead != 0)) {
 		this->_QueueTail = 0;
 		memcpy(
 			this->_ArrayLocation + this->_QueueTail,
 			&item,
 			sizeof(item)
 		);
-		this->_ArraySize++;
 		return true;
 	}
-	else
-	{
+	else {
 		this->_QueueTail++;
 		memcpy(
 			this->_ArrayLocation + this->_QueueTail,
 			&item,
 			sizeof(item)
 		);
-		this->_ArraySize++;
 	}
-
+	
 }
-
-template<class _ElemType>
-inline void circularQueue<_ElemType>::rotate() noexcept{
-	this->enqueue(this->dequeue());
-}
-
 
 template<class _ElemType>
 inline _ElemType circularQueue<_ElemType>::dequeue() noexcept
@@ -155,7 +108,6 @@ inline _ElemType circularQueue<_ElemType>::dequeue() noexcept
 		this->_QueueHead = 0;
 	else
 		this->_QueueHead++;
-	this->_ArraySize--;
 
 	return data;
 }
@@ -171,7 +123,5 @@ inline _ElemType circularQueue<_ElemType>::operator[](size_t i) noexcept
 
 	return *(this->_ArrayLocation + i);
 }
-
-_STDLIB_END
 
 #endif /*QUEUE_IMPL*/
