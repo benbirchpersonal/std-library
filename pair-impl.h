36b06fbeb27509665cb480485093c74ff3fae9d8
benbirchpersonal <93481691+benbirchpersonal@users.noreply.github.com>
2022-03-15 22:47:20 +0000

    Update pair-impl.h

--- pair-impl.h
+++ pair-impl.h
@@ -24,41 +24,39 @@ pair<int, const char*> x = {1,"test"};
 */
 
 _STDLIB_BEGIN
-
-template <class _ElemType1 , class _ElemType2>
+template <class _ElemType1, class _ElemType2>
 class pair
 {
 public:
-								pair();
-								pair( const _ElemType1& , const _ElemType2& );
-								~pair();
+	pair();
+	pair(const _ElemType1&, const _ElemType2&);
+	~pair();
 
-			const void 			operator=( const pair<_ElemType1 , _ElemType2>& copy );
-_NOEXCEPT	const _ElemType1&	first();
-_NOEXCEPT	const _ElemType2&	second();
+	const void	 operator=(const pair<_ElemType1, _ElemType2>& copy);
 
-	const pair<_ElemType1 , _ElemType2>& swap() noexcept;
+	void pair<_ElemType1, _ElemType2>::swap() noexcept;
 
 
 
-private:
-	_ElemType1* _first;
-	_ElemType2* _second;
+	_ElemType1* first;
+	_ElemType2* second;
+
 };
 
 /*
 Defualt constructor for pair. allocates space for 2 objects but leaves uninitialzied.
 
 */
-template <class _ElemType1 , class _ElemType2>
-pair<_ElemType1 , _ElemType2>::pair ()
+template <class _ElemType1, class _ElemType2>
+pair<_ElemType1, _ElemType2>::pair()
 {
-	assert ( sizeof ( _ElemType1 ) > 0 && sizeof ( _ElemType2 ) > 0 );
-	this->_first = ( _ElemType1* ) malloc ( sizeof ( _ElemType1 ) + sizeof ( _ElemType2 ) );
-	this->_second = ( _ElemType2 ) ( this->_first + 1 );
+	
+	assert(sizeof(_ElemType1) > 0 && sizeof(_ElemType2) > 0);
+	this->first = (_ElemType1*)malloc(sizeof(_ElemType1) + sizeof(_ElemType2));
+	this->second = (_ElemType2*)(this->first + 1);
 
 #ifdef DEBUG
-	printf ( "created empty pair at %p" , this->_first );
+	printf("created empty pair at %p", this->first);
 #endif
 
 }
@@ -68,73 +66,54 @@ pair<_ElemType1 , _ElemType2>::pair ()
 Pair constructor with "initializer list" of 2 objects, can be initialized with {x,y} or (x,y).
 Allocates and copies objects into the pair
 */
-template <class _ElemType1 , class _ElemType2>
-pair<_ElemType1 , _ElemType2>::pair ( const _ElemType1& obj1 , const _ElemType2& obj2 )
+template <class _ElemType1, class _ElemType2>
+pair<_ElemType1, _ElemType2>::pair(const _ElemType1& obj1, const _ElemType2& obj2)
 {
-	assert ( sizeof ( _ElemType1 ) > 0 && sizeof ( _ElemType2 ) > 0 );
-	this->_first = ( _ElemType1* ) malloc ( sizeof ( _ElemType1 ) + sizeof ( _ElemType2 ) );
-	this->_second = ( _ElemType2* ) ( this->_first + 1 );
-	memcpy_s (
-		( void* ) this->_first ,
-		sizeof ( _ElemType1 ) ,
-		( const void* ) &obj1 ,
-		sizeof ( _ElemType1 )
+	assert(sizeof(_ElemType1) > 0 && sizeof(_ElemType2) > 0);
+	this->first = (_ElemType1*)malloc(sizeof(_ElemType1) + sizeof(_ElemType2));
+	this->second = (_ElemType2*)(this->first + 1);
+	memcpy_s(
+		(void*)this->first,
+		sizeof(_ElemType1),
+		(const void*)&obj1,
+		sizeof(_ElemType1)
 	);
 
-	memcpy_s (
-		( void* ) this->_second ,
-		sizeof ( _ElemType2 ) ,
-		( const void* ) &obj2 ,
-		sizeof ( _ElemType2 )
+	memcpy_s(
+		(void*)this->second,
+		sizeof(_ElemType2),
+		(const void*)&obj2,
+		sizeof(_ElemType2)
 	);
 
 #ifdef DEBUG
-	printf ( "copied elements to %p" , this->_first );
+	printf("copied elements to %p", this->first);
 #endif
 }
 
 /*
 Destructor for pair class. prints debug message when destructor is called.
 */
-template <class _ElemType1 , class _ElemType2>
-pair<_ElemType1 , _ElemType2>::~pair ()
+template <class _ElemType1, class _ElemType2>
+pair<_ElemType1, _ElemType2>::~pair()
 {
 #ifdef DEBUG
-	printf ( "out of scope at %p" , this->_first );
+	printf("out of scope at %p", this->first);
 #endif
 
-}
-
-
-/*
-returns a const reference to the first object in the pair
-*/
-template <class _ElemType1 , class _ElemType2>
-const _ElemType1& pair<_ElemType1 , _ElemType2>::first ()
-{
-	return *this->_first;
-}
-
-/*
-returns a const reference to the second object in the pair
-*/
-template <class _ElemType1 , class _ElemType2>
-const _ElemType2& pair<_ElemType1 , _ElemType2>::second ()
-{
-	return *this->_second;
 }
 
 /*
 assignment operator with const pair<>& containing the same types.
 */
-template <class _ElemType1 , class _ElemType2>
-const void pair<_ElemType1 , _ElemType2>::operator=( const pair<_ElemType1 , _ElemType2>& copy )
+template <class _ElemType1, class _ElemType2>
+const void pair<_ElemType1, _ElemType2>::operator=(const pair<_ElemType1, _ElemType2>& copy)
 {
-	memcpy_s (
-		this ,
-		sizeof ( this ) ,
-		copy ,
-		sizeof ( this )
+	memcpy_s(
+		this,
+		sizeof(this),
+		copy,
+		sizeof(this)
 	);
 }
 
@@ -146,36 +125,39 @@ very bad coding practices and unreadable and difficult to understand code.
 
 Implementation pretty simple, create temp and swap items and then free temp.
 */
-template <class _ElemType1 , class _ElemType2>
-const pair<_ElemType1 , _ElemType2> pair<_ElemType1 , _ElemType2>::swap () & noexcept
+template <class _ElemType1, class _ElemType2>
+void pair<_ElemType1, _ElemType2>::swap() noexcept
 {
-	_ElemType1* temp = ( _ElemType1* ) malloc ( sizeof ( _ElemType1 ) );
-	assert ( is_same<_ElemType1 , _ElemType2>::value );				// objects of different types may not be swapped.
-	memcpy_s (
-		temp ,
-		sizeof ( _ElemType1 ) ,
-		this->_first ,
-		sizeof ( _ElemType1 )
+
+	_ElemType1* temp = (_ElemType1*)malloc(sizeof(_ElemType1));
+
+	memcpy_s(
+		temp,
+		sizeof(_ElemType1),
+		&this->first,
+		sizeof(_ElemType1)
 	);
 
-	memcpy_s (
-		this->_first ,
-		sizeof ( _ElemType1 ) ,
-		this->_second ,
-		sizeof ( _ElemType1 )
+	memcpy_s(
+		&this->first,
+		sizeof(_ElemType1),
+		&this->second,
+		sizeof(_ElemType1)
 	);
 
-	memcpy_s (
-		this->_second ,
-		sizeof ( _ElemType1 ) ,
-		temp ,
-		sizeof ( _ElemType1 )
+	memcpy_s(
+		&this->second,
+		sizeof(_ElemType1),
+		temp,
+		sizeof(_ElemType1)
 	);
-	free ( temp );
-	return *this;
+	free(temp);
+	return;
 
 }
 
 _STDLIB_END
 
 #endif // PAIR_INCLUDED
+
+
